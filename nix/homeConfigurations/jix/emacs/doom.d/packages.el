53ff3c4df226446fda9cbcf3e6dd08f3c18e9172
Jamie van Dyke <me@fearof.fish>
2023-01-30 15:52:35 +0000

    move to literate config in emacs

--- nix/homeConfigurations/jix/emacs/doom.d/packages.el
+++ nix/homeConfigurations/jix/emacs/doom.d/packages.el
@@ -1,79 +1,37 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
+(package! aggressive-indent)
 
-;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect -- or
-;; use 'M-x doom/reload'.
+(package! evil-cleverparens)
 
+(package! which-key-posframe)
 
-;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
+(package! evil-escape :disable t)
 
-;; To install a package directly from a remote git repo, you must specify a
-;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
-;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
+(package! evil-matchit)
 
-;; If the package you are trying to install does not contain a PACKAGENAME.el
-;; file, or is located in a subdirectory of the repo, you'll need to specify
-;; `:files' in the `:recipe':
-;(package! this-package
-;  :recipe (:host github :repo "username/repo"
-;           :files ("some-file.el" "src/lisp/*.el")))
+(package! evil-numbers :disable t)
 
-;; If you'd like to disable a package included with Doom, you can do so here
-;; with the `:disable' property:
-;(package! builtin-package :disable t)
+(package! plus-minus
+  :recipe (:host github
+           :repo "peterwu/plus-minus"))
 
-;; You can override the recipe of a built in package without having to specify
-;; all the properties for `:recipe'. These will inherit the rest of its recipe
-;; from Doom or MELPA/ELPA/Emacsmirror:
-;(package! builtin-package :recipe (:nonrecursive t))
-;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+(package! string-inflection)
 
-;; Specify a `:branch' to install a package from a particular branch or tag.
-;; This is required for some packages whose default branch isn't 'master' (which
-;; our package manager can't deal with; see raxod502/straight.el#279)
-;(package! builtin-package :recipe (:branch "develop"))
+(package! page-break-lines)
 
-;; Use `:pin' to specify a particular commit to install.
-;(package! builtin-package :pin "1a2b3c4d5e")
+(package! info-plus :pin "5ecd4c1e3f1384bffb386e61a8e3788652e55c25")
 
+(if (getenv "GITHUB_WORKFLOW")
+    (package! gitmoji
+      :recipe (:host github
+               :repo "Townk/gitmoji"))
+  (package! gitmoji
+    :recipe (:local-repo "~/workplace/personal/emacs/gitmoji")))
 
-;; Doom's packages are pinned to a specific commit and updated from release to
-;; release. The `unpin!' macro allows you to unpin single packages...
-;(unpin! pinned-package)
-;; ...or multiple packages
-;(unpin! pinned-package another-pinned-package)
-;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
-;(unpin! t)
+(package! dired-collapse)
+(package! dired-narrow)
+(package! dired-ranger)
+(package! dired-quick-sort)
+(package! dired-avfs)
+(package! peep-dired)
 
-
-;; https://github.com/riscy/shx-for-emacs
-(package! shx :pin "09ae212")
-
-;; https://github.com/emacsorphanage/god-mode
-(package! god-mode :pin "1d7d647")
-
-;; https://github.com/gridaphobe/evil-god-state
-(package! evil-god-state :pin "3d44197")
-
-;; https://github.com/paulalesius/doom-golden-ratio
-(package! doom-golden-ratio
-  :recipe
-  (:host github
-    :repo "vic/doom-golden-ratio"))
-
-;; https://github.com/mclear-tools/bespoke-themes
-(package! bespoke-themes
-  :recipe
-  (:host github :repo "mclear-tools/bespoke-themes" :branch "main"
-    ;; :pin "4378932"
-    ))
-
-(package! copilot
-  :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
-
-(package! emacs-mini-frame
-  :recipe (:host github :repo "muffinmad/emacs-mini-frame" :files ("*.el")))
+(package! esh-autosuggest)
