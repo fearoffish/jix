dc3c1f6ce5dc27ab5ec2bbfa1c7b544b2b2a12cf
Jamie van Dyke <jamievandyke@fearofair.localdomain>
2023-03-03 15:03:53 +0000

    trying to get 1password as my ssh agent

--- nix/homeConfigurations/jix/ssh/default.nix
+++ nix/homeConfigurations/jix/ssh/default.nix
@@ -1,18 +1,19 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{ config, lib, pkgs, ... }: {
   programs.ssh = {
     enable = true;
     controlMaster = "auto";
     controlPersist = "10m";
     forwardAgent = true;
+
+    matchBlocks = {
+      "box.fearof.fish" = {
+        hostname = "172.105.133.7";
+        identityAgent = "~/.1password/agent.sock";
+      };
+    };
   };
   # TODO: handle this with nix not manually
   # home.file = {
   #   ".ssh/id_rsa".source = "${config.dots}/ssh/id_rsa";
   #   ".ssh/id_rsa.pub".source = ./id_rsa.pub;
-  # };
 }
