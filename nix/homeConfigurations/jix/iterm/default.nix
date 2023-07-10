44b6a414f166c7226eb523fcb2aeb4255c524400
Jamie van Dyke <me@fearof.fish>
2023-07-10 13:13:38 +0100

    move iterm config

--- nix/homeConfigurations/jix/iterm/default.nix
+++ nix/homeConfigurations/jix/iterm/default.nix
@@ -1,10 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
-}: {
-  home.file."/Library/Preferences/com.googlecode.iterm2.plist".source =
+{ config, lib, pkgs, inputs, ... }: {
+  home.file.".config/iterm/com.googlecode.iterm2.plist".source =
     ./com.googlecode.iterm2.plist;
 }
